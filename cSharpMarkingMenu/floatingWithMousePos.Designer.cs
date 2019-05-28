2fd98bfd49f62f4db8cd6620be17c1d558eeec21
Hasan SEZER <vimevim@users.noreply.github.com>
2019-05-28 14:28:18 +0300

    Görüntüsü düzeltildi

--- cSharpMarkingMenu/floatingWithMousePos.Designer.cs
+++ cSharpMarkingMenu/floatingWithMousePos.Designer.cs
@@ -28,9 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FloatingWithMousePos));
             this.lblUp = new System.Windows.Forms.Label();
-            this.lblCenter = new System.Windows.Forms.Label();
             this.lblUpRight = new System.Windows.Forms.Label();
             this.lblMidRight = new System.Windows.Forms.Label();
             this.lblMidLeft = new System.Windows.Forms.Label();
@@ -43,126 +41,104 @@
             // lblUp
             // 
             this.lblUp.AutoSize = true;
-            this.lblUp.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.lblUp.Enabled = false;
-            this.lblUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblUp.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lblUp.BackColor = System.Drawing.Color.Black;
+            this.lblUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblUp.ForeColor = System.Drawing.Color.White;
             this.lblUp.Location = new System.Drawing.Point(201, 14);
             this.lblUp.Name = "lblUp";
-            this.lblUp.Size = new System.Drawing.Size(108, 48);
+            this.lblUp.Size = new System.Drawing.Size(67, 29);
             this.lblUp.TabIndex = 33;
             this.lblUp.Text = "copy";
-            // 
-            // lblCenter
-            // 
-            this.lblCenter.AutoSize = true;
-            this.lblCenter.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.lblCenter.Enabled = false;
-            this.lblCenter.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblCenter.ForeColor = System.Drawing.Color.DarkOliveGreen;
-            this.lblCenter.Location = new System.Drawing.Point(201, 201);
-            this.lblCenter.Name = "lblCenter";
-            this.lblCenter.Size = new System.Drawing.Size(48, 48);
-            this.lblCenter.TabIndex = 34;
-            this.lblCenter.Text = "X";
-            this.lblCenter.Visible = false;
             // 
             // lblUpRight
             // 
             this.lblUpRight.AutoSize = true;
-            this.lblUpRight.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.lblUpRight.Enabled = false;
-            this.lblUpRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblUpRight.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lblUpRight.BackColor = System.Drawing.Color.Black;
+            this.lblUpRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblUpRight.ForeColor = System.Drawing.Color.White;
             this.lblUpRight.Location = new System.Drawing.Point(352, 50);
             this.lblUpRight.Name = "lblUpRight";
-            this.lblUpRight.Size = new System.Drawing.Size(84, 48);
+            this.lblUpRight.Size = new System.Drawing.Size(51, 29);
             this.lblUpRight.TabIndex = 35;
             this.lblUpRight.Text = "twit";
             // 
             // lblMidRight
             // 
             this.lblMidRight.AutoSize = true;
-            this.lblMidRight.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.lblMidRight.Enabled = false;
-            this.lblMidRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblMidRight.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lblMidRight.BackColor = System.Drawing.Color.Black;
+            this.lblMidRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblMidRight.ForeColor = System.Drawing.Color.White;
             this.lblMidRight.Location = new System.Drawing.Point(388, 201);
             this.lblMidRight.Name = "lblMidRight";
-            this.lblMidRight.Size = new System.Drawing.Size(112, 48);
+            this.lblMidRight.Size = new System.Drawing.Size(69, 29);
             this.lblMidRight.TabIndex = 36;
             this.lblMidRight.Text = "undo";
             // 
             // lblMidLeft
             // 
             this.lblMidLeft.AutoSize = true;
-            this.lblMidLeft.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.lblMidLeft.Enabled = false;
-            this.lblMidLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblMidLeft.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lblMidLeft.BackColor = System.Drawing.Color.Black;
+            this.lblMidLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblMidLeft.ForeColor = System.Drawing.Color.White;
             this.lblMidLeft.Location = new System.Drawing.Point(14, 201);
             this.lblMidLeft.Name = "lblMidLeft";
-            this.lblMidLeft.Size = new System.Drawing.Size(103, 48);
+            this.lblMidLeft.Size = new System.Drawing.Size(63, 29);
             this.lblMidLeft.TabIndex = 37;
             this.lblMidLeft.Text = "redo";
             // 
             // lblUpLeft
             // 
             this.lblUpLeft.AutoSize = true;
-            this.lblUpLeft.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.lblUpLeft.Enabled = false;
-            this.lblUpLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblUpLeft.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lblUpLeft.BackColor = System.Drawing.Color.Black;
+            this.lblUpLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblUpLeft.ForeColor = System.Drawing.Color.White;
             this.lblUpLeft.Location = new System.Drawing.Point(50, 50);
             this.lblUpLeft.Name = "lblUpLeft";
-            this.lblUpLeft.Size = new System.Drawing.Size(178, 48);
+            this.lblUpLeft.Size = new System.Drawing.Size(109, 29);
             this.lblUpLeft.TabIndex = 38;
             this.lblUpLeft.Text = "google it";
             // 
             // lblDownRight
             // 
             this.lblDownRight.AutoSize = true;
-            this.lblDownRight.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.lblDownRight.Enabled = false;
-            this.lblDownRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblDownRight.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lblDownRight.BackColor = System.Drawing.Color.Black;
+            this.lblDownRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblDownRight.ForeColor = System.Drawing.Color.White;
             this.lblDownRight.Location = new System.Drawing.Point(352, 352);
             this.lblDownRight.Name = "lblDownRight";
-            this.lblDownRight.Size = new System.Drawing.Size(113, 48);
+            this.lblDownRight.Size = new System.Drawing.Size(70, 29);
             this.lblDownRight.TabIndex = 39;
             this.lblDownRight.Text = "mute";
             // 
             // lblDownMid
             // 
             this.lblDownMid.AutoSize = true;
-            this.lblDownMid.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.lblDownMid.Enabled = false;
-            this.lblDownMid.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblDownMid.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lblDownMid.BackColor = System.Drawing.Color.Black;
+            this.lblDownMid.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblDownMid.ForeColor = System.Drawing.Color.White;
             this.lblDownMid.Location = new System.Drawing.Point(201, 388);
             this.lblDownMid.Name = "lblDownMid";
-            this.lblDownMid.Size = new System.Drawing.Size(122, 48);
+            this.lblDownMid.Size = new System.Drawing.Size(75, 29);
             this.lblDownMid.TabIndex = 40;
             this.lblDownMid.Text = "paste";
             // 
             // lblDownLeft
             // 
             this.lblDownLeft.AutoSize = true;
-            this.lblDownLeft.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.lblDownLeft.Enabled = false;
-            this.lblDownLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblDownLeft.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lblDownLeft.BackColor = System.Drawing.Color.Black;
+            this.lblDownLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblDownLeft.ForeColor = System.Drawing.Color.White;
             this.lblDownLeft.Location = new System.Drawing.Point(50, 352);
             this.lblDownLeft.Name = "lblDownLeft";
-            this.lblDownLeft.Size = new System.Drawing.Size(150, 48);
+            this.lblDownLeft.Size = new System.Drawing.Size(91, 29);
             this.lblDownLeft.TabIndex = 41;
             this.lblDownLeft.Text = "refresh";
             // 
             // FloatingWithMousePos
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackColor = System.Drawing.Color.White;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackgroundImage = global::cSharpMarkingMenu.Properties.Resources.JoystickOutline_fw;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(450, 450);
             this.Controls.Add(this.lblDownLeft);
@@ -172,9 +148,9 @@
             this.Controls.Add(this.lblMidLeft);
             this.Controls.Add(this.lblMidRight);
             this.Controls.Add(this.lblUpRight);
-            this.Controls.Add(this.lblCenter);
             this.Controls.Add(this.lblUp);
             this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FloatingWithMousePos";
             this.ShowIcon = false;
@@ -193,7 +169,6 @@
         #endregion
 
         private System.Windows.Forms.Label lblUp;
-        private System.Windows.Forms.Label lblCenter;
         private System.Windows.Forms.Label lblUpRight;
         private System.Windows.Forms.Label lblMidRight;
         private System.Windows.Forms.Label lblMidLeft;
